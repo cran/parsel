2a3d1a23853d02d66cdbc3dbd154f5911be68590
Till Tietz <ttietz2014@gmail.com>
2022-06-08 16:40:02 +0000

    version 0.2.0

---
+++ R/constructors_elements.R
@@ -0,0 +1,378 @@
+#' utility function to check for repeated and generate unique variable names
+#'
+#' @param input character string
+#' @return  generated variable name as a character string
+#' @keywords internal
+
+
+gen_varname <- function(input){
+
+  cont <- TRUE
+  count <- 1
+
+  while(cont){
+
+    var <- paste("parsel_var", count, sep = "_")
+    cont <- grepl(paste("\\b",var,"\\b", sep = ""), input)
+
+    count <- count + 1
+
+  }
+
+  return(var)
+
+}
+
+
+#' wrapper around clickElement() method to generate safe scraping code
+#'
+#' @param using character string specifying locator scheme to use to search elements. Available schemes: "class name", "css selector", "id", "name", "link text", "partial link text", "tag name", "xpath".
+#' @param value character string specifying the search target.
+#' @param name character string specifying the object name the RSelenium "wElement" class object should be saved to.
+#' @param new_page logical indicating if clickElement() action will result in a change in url.
+#' @param prev a placeholder for the output of functions being piped into click(). Defaults to NULL and should not be altered.
+#' @return a character string defining 'RSelenium' clicking instructions that can be pasted into a scraping function.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'
+#' #navigate to wikipedia, click random article
+#'
+#' parsel::go("https://www.wikipedia.org/") %>>%
+#' parsel::click(using = "id", value = "n-randompage") %>>%
+#' show()
+#'
+#' }
+
+click <- function(using, value, name = NULL, new_page = FALSE, prev = NULL){
+
+  if(!missing(using)){
+    if(!is.character(using)){
+      stop("using is not of type character")
+    }
+  }
+
+  if(!missing(value)){
+    if(!is.character(value)){
+      stop("value is not of type character")
+    }
+  }
+
+  if(!is.null(name)){
+    if(!is.character(name)){
+      stop("name is not of type character")
+    }
+  }
+
+  if(is.null(name)){
+
+    if(is.null(prev)){
+      name <- gen_varname("")
+    } else {
+      name <- gen_varname(prev)
+    }
+
+  }
+
+  if(!is.logical(new_page)){
+    stop("new_page is not of type logical")
+  }
+
+
+  finding <- paste(name, " <- ","remDr$findElement(using = '", using,"', '", value, "')", sep = "")
+  clicking <- paste(name,"$clickElement()", sep = "")
+
+  if(new_page){
+
+    from <- "from <- seleniumPipes::getCurrentUrl(remDr)"
+
+    wait <- paste("not_changed <- TRUE",
+                  "while(not_changed){",
+                  "Sys.sleep(0.25)",
+                  "current <- seleniumPipes::getCurrentUrl(remDr)",
+                  "if(current != from){",
+                  "not_changed <- FALSE",
+                  "}",
+                  "}",
+                  sep = "\n")
+
+    out <- paste(finding, from, clicking, wait, sep = "\n")
+
+  } else {
+
+    wait <- "Sys.sleep(0.25)"
+
+    out <- paste(finding, clicking, wait, sep = "\n")
+
+  }
+
+  if(!is.null(prev)){
+    out <- paste(prev, out, sep = " \n \n ")
+  }
+
+  return(out)
+
+}
+
+
+
+
+#' wrapper around sendKeysToElement() method to generate safe scraping code
+#'
+#' @param using character string specifying locator scheme to use to search elements. Available schemes: "class name", "css selector", "id", "name", "link text", "partial link text", "tag name", "xpath".
+#' @param value character string specifying the search target.
+#' @param name character string specifying the object name the RSelenium "wElement" class object should be saved to.If NULL a name will be generated automatically.
+#' @param text a character vector specifying the text to be typed.
+#' @param text_object a character string specifying the name of an external object holding the text to be typed. Note that the remDr$sendKeysToElement method only accepts list inputs.
+#' @param new_page logical indicating if sendKeysToElement() action will result in a change in url.
+#' @param prev a placeholder for the output of functions being piped into type(). Defaults to NULL and should not be altered.
+#' @return a character string defining 'RSelenium' typing instructions that can be pasted into a scraping function.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'
+#' #navigate to wikipedia, type "Hello" into the search box,  press enter
+#'
+#' parsel::go("https://www.wikipedia.org/") %>>%
+#' parsel::type(using = "id",
+#'              value = "searchInput",
+#'              name = "searchbox",
+#'              text = c("Hello","\uE007")) %>>%
+#'              show()
+#'
+#' #navigate to wikipeda, type content stored in external object "x" into search box
+#'
+#' parsel::go("https://www.wikipedia.org/") %>>%
+#' parsel::type(using = "id",
+#'              value = "searchInput",
+#'              name = "searchbox",
+#'              text_object = "x") %>>%
+#'              show()
+#'
+#' }
+
+type <- function(using, value, name = NULL, text, text_object, new_page = FALSE, prev = NULL){
+
+  if(!missing(using)){
+    if(!is.character(using)){
+      stop("using is not of type character")
+    }
+  }
+
+  if(!missing(value)){
+    if(!is.character(value)){
+      stop("value is not of type character")
+    }
+  }
+
+  if(!is.null(name)){
+    if(!is.character(name)){
+      stop("name is not of type character")
+    }
+  }
+
+  if(!missing(text) & !missing(text_object)){
+    stop("please only define one of text or text_object")
+  }
+
+
+  if(!missing(text)){
+
+    if(!is.character(text)){
+      stop("text is not of type character")
+    }
+
+    text <- paste("'",text,"'", sep = "")
+
+    if(length(text) > 1){
+      text <- paste(text, collapse = ",")
+    }
+
+  }
+
+  if(!missing(text_object)){
+
+    if(length(text_object) > 1){
+      stop("cannot define multiple text objects. please define a single text_object.")
+    }
+
+    if(!is.character(text_object)){
+      stop("text_object is not of type character")
+    }
+
+  }
+
+  defined <- which(c(!missing(text),!missing(text_object)))
+
+  if(is.null(name)){
+
+    if(is.null(prev)){
+      name <- gen_varname("")
+    } else {
+      name <- gen_varname(prev)
+    }
+
+  }
+
+  if(!is.logical(new_page)){
+    stop("new_page is not of type logical")
+  }
+
+
+  finding <- paste(name, " <- ","remDr$findElement(using = '", using,"', '", value, "')", sep = "")
+
+  if(defined == 1){
+
+    typing <- paste(name,"$sendKeysToElement(list(",text,"))", sep = "")
+
+  } else {
+
+    typing <- paste(name,"$sendKeysToElement(",text_object,")", sep = "")
+
+  }
+
+  if(new_page){
+
+    from <- "from <- seleniumPipes::getCurrentUrl(remDr)"
+
+    wait <- paste("not_changed <- TRUE",
+                  "while(not_changed){",
+                  "Sys.sleep(0.25)",
+                  "current <- seleniumPipes::getCurrentUrl(remDr)",
+                  "if(current != from){",
+                  "not_changed <- FALSE",
+                  "}",
+                  "}",
+                  sep = "\n")
+
+    out <- paste(finding, from, typing, wait, sep = "\n")
+
+  } else {
+
+    wait <- "Sys.sleep(0.25)"
+
+    out <- paste(finding, typing, wait, sep = "\n")
+
+  }
+
+  if(!is.null(prev)){
+    out <- paste(prev, out, sep = " \n \n ")
+  }
+
+  return(out)
+
+}
+
+
+#' wrapper around getElementText() method to generate safe scraping code
+#'
+#' @param using character string specifying locator scheme to use to search elements. Available schemes: "class name", "css selector", "id", "name", "link text", "partial link text", "tag name", "xpath".
+#' @param value character string specifying the search target.
+#' @param name character string specifying the object name the RSelenium "wElement" class object should be saved to. If NULL a name will be generated automatically.
+#' @param multiple logical indicating whether multiple elements should be returned. If TRUE the findElements() method will be invoked.
+#' @param prev a placeholder for the output of functions being piped into get_element(). Defaults to NULL and should not be altered.
+#' @return a character string defining 'RSelenium' getElementText() instructions that can be pasted into a scraping function.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'
+#' #navigate to wikipedia, type "Hello" into the search box,
+#' #press enter, get page header
+#'
+#' parsel::go("https://www.wikipedia.org/") %>>%
+#' parsel::type(using = "id",
+#'              value = "searchInput",
+#'              name = "searchbox",
+#'              text = c("Hello","\uE007")) %>>%
+#' parsel::get_element(using = "id",
+#'                     value = "firstHeading",
+#'                     name = "header") %>>%
+#'             show()
+#'
+#' #navigate to wikipedia, type "Hello" into the search box, press enter,
+#' #get page header, save in external data.frame x.
+#'
+#' parsel::go("https://www.wikipedia.org/") %>>%
+#' parsel::type(using = "id",
+#'              value = "searchInput",
+#'              name = "searchbox",
+#'              text = c("Hello","\uE007")) %>>%
+#' parsel::get_element(using = "id",
+#'                     value = "firstHeading",
+#'                     name = "x[,1]") %>>%
+#'                     show()
+#'
+#' }
+
+get_element <- function(using, value, name = NULL, multiple = FALSE, prev = NULL){
+
+  if(!missing(using)){
+    if(!is.character(using)){
+      stop("using is not of type character")
+    }
+  }
+
+  if(!missing(value)){
+    if(!is.character(value)){
+      stop("value is not of type character")
+    }
+  }
+
+  if(!is.null(name)){
+    if(!is.character(name)){
+      stop("name is not of type character")
+    }
+  }
+
+  if(!is.logical(multiple)){
+    stop("multiple not of class logical")
+  }
+
+  if(is.null(name)){
+
+    if(is.null(prev)){
+      name <- gen_varname("")
+    } else {
+      name <- gen_varname(prev)
+    }
+
+  }
+
+
+  if(multiple){
+
+    finding <- paste(name, " <- ", "try(", "remDr$findElements(using = '", using,"', '", value, "')", ")", sep = "")
+
+    out <- paste(finding,
+                 paste("if(is(", name, ",'try-error')){", sep = ""),
+                 paste(name, " <- NA", sep = ""),
+                 "} else {",
+                 paste(name, " <- ", "lapply(", name, ", function(i) ","i$getElementText())", sep = ""),
+                 "}",
+                 sep = " \n")
+
+  } else {
+
+    finding <- paste(name, " <- ", "try(", "remDr$findElement(using = '", using,"', '", value, "')", ")", sep = "")
+
+    out <- paste(finding,
+                 paste("if(is(", name, ",'try-error')){", sep = ""),
+                 paste(name, " <- NA", sep = ""),
+                 "} else {",
+                 paste(name, " <- ", name,"$getElementText()", sep = ""),
+                 "}",
+                 sep = " \n")
+
+  }
+
+  if(!is.null(prev)){
+    out <- paste(prev, out, sep = " \n \n ")
+  }
+
+  return(out)
+
+}
+
--- README.md
+++ README.md
@@ -6,6 +6,9 @@
 <!-- badges: start -->
 
 [![R-CMD-check](https://github.com/till-tietz/parsel/workflows/R-CMD-check/badge.svg)](https://github.com/till-tietz/parsel/actions)
+[![CRAN
+status](https://www.r-pkg.org/badges/version/parsel)](https://CRAN.R-project.org/package=parsel)
+![](https://cranlogs.r-pkg.org/badges/grand-total/parsel?color)
 <!-- badges: end -->
 
 `parsel` is a framework for parallelized dynamic web-scraping using
@@ -15,19 +18,24 @@ simultaneously, thus greatly increasing the efficiency of your scraping.
 `parsel` utilizes chunked input processing as well as error catching and
 logging, to ensure seamless execution of your scraping routine and
 minimal data loss, even in the presence of unforeseen `RSelenium`
-errors.
+errors. `parsel` additionally provides convenient wrapper functions
+around RSelenium methods, that allow you to quickly generate safe
+scraping code with minimal coding on your end.
 
 ## Installation
 
-You can install the development version of `parsel` from
-[GitHub](https://github.com/) with:
-
 ``` r
+# Install parsel from CRAN
+install.packages("parsel")
+
+# Or the development version from GitHub:
 # install.packages("devtools")
 devtools::install_github("till-tietz/parsel")
 ```
 
-## Example
+## Usage
+
+### Parallel Scraping
 
 The following example will hopefully serve to illustrate the
 functionality and ideas behind how `parsel` operates. We’ll set up the
@@ -56,7 +64,7 @@ library(RSelenium)
 library(parsel)
 
 #let's define our scraping function input 
-#we want to run our function 8 times and we want it to start on the wikipedia main page each time 
+#we want to run our function 4 times and we want it to start on the wikipedia main page each time 
 input <- rep("https://de.wikipedia.org",4)
 
 #let's define our scraping function 
@@ -115,8 +123,10 @@ get_wiki_text <- function(x){
 ```
 
 Now that we have our scrape function and input we can parallelize the
-execution of the function. `parscrape` will show a progress bar, as well
-as elapsed and estimated remaining time so you can keep track of
+execution of the function. For speed and efficiency reasons, it is
+advisable to specify the headless browser option in the
+`extraCapabilities` argument. `parscrape` will show a progress bar, as
+well as elapsed and estimated remaining time so you can keep track of
 scraping progress.
 
 ``` r
@@ -125,7 +135,10 @@ wiki_text <- parsel::parscrape(scrape_fun = get_wiki_text,
                                cores = 2,
                                packages = c("RSelenium","XML"),
                                browser = "firefox",
-                               scrape_tries = 1)
+                               scrape_tries = 1,
+                               extraCapabilities = list(
+                                     "moz:firefoxOptions" = list(args = list('--headless'))
+                                     ))
 ```
 
 `parscrape` returns a list with two elements:
@@ -133,3 +146,60 @@ wiki_text <- parsel::parscrape(scrape_fun = get_wiki_text,
 1.  a list of your scrape function output
 2.  a data.frame of inputs it was unable to scrape, and the associated
     error messages
+
+### RSelenium Constructors
+
+`parsel` allows you to generate scraping code and output it to the
+console via its wrapper functions around RSelenium methods. We’ll
+reproduce a slightly stripped down version of the RSelenium code in the
+above wikipedia scraping routine via the `parsel` `constructor`
+`functions`.
+
+``` r
+library(parsel)
+
+go(url = "x") %>>%
+  click(using = "xpath", value = "/html/body/div[5]/div[2]/nav[1]/div/ul/li[3]/a", name = "rand_art") %>>%
+  get_element(using = "id", value = "firstHeading", name = "title") %>>%
+  click(using = "xpath", value = "/html/body/div[3]/div[3]/div[5]/div[1]/p[1]/a[1]", name = "link") %>>%
+  get_element(using = "id", value = "firstHeading", name = "first_link_title") %>>%
+  get_element(using = "xpath", value = "/html/body/div[3]/div[3]/div[5]/div[1]/p[1]", name = "first_link_text") %>>%
+  show()
+#> # navigate to url
+#> not_loaded <- TRUE
+#> remDr$navigate('x')
+#> while(not_loaded){
+#> Sys.sleep(0.25)
+#> current <- seleniumPipes::getCurrentUrl(remDr)
+#> if(current == 'x'){
+#> not_loaded <- FALSE
+#> }
+#> } 
+#>  
+#>  rand_art <- remDr$findElement(using = 'xpath', '/html/body/div[5]/div[2]/nav[1]/div/ul/li[3]/a')
+#> rand_art$clickElement() 
+#>  
+#>  title <- try(remDr$findElement(using = 'id', 'firstHeading')) 
+#> if(is(title,'try-error')){ 
+#> title <- NA 
+#> } else { 
+#> title <- title$getElementText() 
+#> } 
+#>  
+#>  link <- remDr$findElement(using = 'xpath', '/html/body/div[3]/div[3]/div[5]/div[1]/p[1]/a[1]')
+#> link$clickElement() 
+#>  
+#>  first_link_title <- try(remDr$findElement(using = 'id', 'firstHeading')) 
+#> if(is(first_link_title,'try-error')){ 
+#> first_link_title <- NA 
+#> } else { 
+#> first_link_title <- first_link_title$getElementText() 
+#> } 
+#>  
+#>  first_link_text <- try(remDr$findElement(using = 'xpath', '/html/body/div[3]/div[3]/div[5]/div[1]/p[1]')) 
+#> if(is(first_link_text,'try-error')){ 
+#> first_link_text <- NA 
+#> } else { 
+#> first_link_text <- first_link_text$getElementText() 
+#> }
+```
